c4eaa5c14aed574c03553157400eec86e26a0b34
sunbitterns <lojeremia@gmail.com>
2022-02-21 11:01:37 -0500

    adding/deleting item functionality

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-< script src = "main.js"
-type = "text/javascript" > < /script>
-
 function addNewList() {
     alert('hello world alert!');
     console.log('hello world console');
+}
+
+function onAddItem() {
+    let list = document.getElementById("grocery-list");
+    let itemInput = document.getElementById("text_input");
+    let newItem = document.createElement("li");
+
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+    console.log(item);
+    item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
 }
